87229f5ddac8925cc7c5ca13a1fa0b81b8302fbb
FarhanSiddiqui2333 <farhansiddiqui2333@gmail.com>
2025-02-21 22:52:23 +0500

    create a about page and add header & footer section on it

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
         <>
             <Nav />
             <NavLink />
-            <div style={{color:'#0f172a', display: 'flex', gap: '40px', padding: '40px', justifyContent: 'space-around', textAlign: 'center' }}>
+            <div style={{ color: '#0f172a', display: 'flex', gap: '40px', padding: '40px', justifyContent: 'space-around', textAlign: 'center' }}>
                 <div style={{ width: '40%', height: 'auto', boxSizing: 'border-box', padding: '20px 40px', borderRight: '1px solid #e2e6e9', borderBottom: '1px solid #e2e6e9', boxShadow: '5px 5px 10px #e2e6e9 ,-5px -5px 80px #e2e6e9 inset, 0px -5px 10px #e2e6e9', backgroundColor: 'white', borderRadius: '12px' }}>
                     <img src={DesignerLogo} alt="logo" style={{ borderRadius: '50%', height: '100px', width: '100px', aspectRatio: '3/4', objectFit: 'cover' }} />
                     <h4 style={{ fontSize: '25px', fontWeight: '800' }}>Designer</h4>
@@ -25,10 +25,10 @@ const About = () => {
                     </p>
                 </div>
             </div>
-            <div style={{color:'#0f172a',width:'80%', height:'auto', padding:'25px 40px', borderTop:'1px solid #e2e6e9', boxShadow:'0px 5px 10px #e2e6e9, 0px 5px 50px #e2e6e9 inset, 0px -5px 20px #e2e6e9', borderRadius:'12px' , textAlign:'center', placeSelf:'center', margin:'40px 0px'}}>
-                <img src={AboutLogo} alt="img" style={{height:'150px', width:'150px', borderRadius:'50%'}} />
-                <h4 style={{fontSize:'25px', fontWeight:'800'}}>About Us</h4>
-                <p style={{lineHeight:'30px', textAlign:'justify'}}>Hello, and welcome!
+            <div style={{ color: '#0f172a', width: '80%', height: 'auto', padding: '25px 40px', borderTop: '1px solid #e2e6e9', boxShadow: '0px 5px 10px #e2e6e9, 0px 5px 50px #e2e6e9 inset, 0px -5px 20px #e2e6e9', borderRadius: '12px', textAlign: 'center', placeSelf: 'center', margin: '40px 0px' }}>
+                <img src={AboutLogo} alt="img" style={{ height: '150px', width: '150px', borderRadius: '50%' }} />
+                <h4 style={{ fontSize: '25px', fontWeight: '800' }}>About Us</h4>
+                <p style={{ lineHeight: '30px', textAlign: 'justify' }}>Hello, and welcome!
 
                     We are a team of passionate individuals dedicated to creating beautiful and functional web experiences. As a frontend developer with a growing passion for design, we specialize in building websites that are not only visually appealing but also user-friendly. Our goal is to bridge the gap between design and development to ensure that each project is crafted to meet the needs of users while maintaining high standards of aesthetics and performance.
 
@@ -37,7 +37,7 @@ const About = () => {
                     Thank you for taking the time to learn a little about us. We look forward to collaborating and creating something amazing together!
 
                 </p>
-                <div style={{display:'flex', justifyContent:'space-between', fontSize:'12px', padding:'20px 20px 0px 20px', color:'#3d3d3d'}}>
+                <div style={{ display: 'flex', justifyContent: 'space-between', fontSize: '12px', padding: '20px 20px 0px 20px', color: '#3d3d3d' }}>
                     <span><i class="fa-solid fa-address-card"></i>  Contact no. : 03473824314</span>
                     <span><i class="fa-brands fa-whatsapp"></i>  Whatsapp : 03473824314</span>
                     <span><i class="fa-solid fa-envelope"></i> Email : farhansiddiqui2333@gmail.com</span>
