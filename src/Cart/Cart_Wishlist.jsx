a4010c767c2a8c41ee5d1d62594ccea7625e7f1b
FarhanSiddiqui2333 <farhansiddiqui2333@gmail.com>
2025-02-22 00:20:22 +0500

    create cart page to show products details and cart functionality

--- src/Cart/Cart_Wishlist.jsx
+++ src/Cart/Cart_Wishlist.jsx
@@ -9,16 +9,16 @@ import { Link } from 'react-router-dom'
 const Cart_Wishlist = () => {
   return (
     <>
-    <Nav />
-    <Navlink />
-    <div style={{display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column', padding:'40px'}}>
-        <span style={{placeSelf:'flex-start', marginLeft:'30px'}}>(0) item in Cart & Wishlist</span>
-        <img src={EmptyBG} alt="" style={{height:'350px'}}/>
-        <h4 style={{color:'#0f172a', fontSize:'25px', fontWeight:'800'}}>Cart List Is Empty</h4>
-        <Link to='/' style={{color:'grey', fontSize:'12px', marginTop:'50px'}}><span>Go Back To Home Page</span></Link>
-    </div>
-    <FeedBack />
-    <Footer />
+      <Nav />
+      <Navlink />
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', padding: '40px' }}>
+        <span style={{ placeSelf: 'flex-start', marginLeft: '30px' }}>(0) item in Cart & Wishlist</span>
+        <img src={EmptyBG} alt="" style={{ height: '350px' }} />
+        <h4 style={{ color: '#0f172a', fontSize: '25px', fontWeight: '800' }}>Cart List Is Empty</h4>
+        <Link to='/' style={{ color: 'grey', fontSize: '12px', marginTop: '50px' }}><span>Go Back To Home Page</span></Link>
+      </div>
+      <FeedBack />
+      <Footer />
     </>
   )
 }
